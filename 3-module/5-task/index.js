e6c09f0bfa2976d05c391114190028232129d628
sonyawender <sonya.wender@gmail.com>
2021-10-11 01:49:39 +0300

    Tasks in Modules 1-3 solved

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,17 @@
 function getMinMax(str) {
-  // ваш код...
+  let result = {
+    min: 0,
+    max: 0
+  };
+
+  let arr = str.split(" ");
+  for (let i = 0; i < arr.length; i++) {
+    let current = Number(arr[i]);
+    if (current < result.min) {
+      result.min = current;
+    } else if (current > result.max) {
+      result.max = current;
+    }
+  }
+  return result;
 }
