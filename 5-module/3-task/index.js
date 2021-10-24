8f75f7ad73cee673ff14efb7f30d94484a5c3ef7
sonyawender <sonya.wender@gmail.com>
2021-10-24 20:41:49 +0300

    Tasks in Modules 4-5 solved

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,34 @@
 function initCarousel() {
-  // ваш код...
+  let rightArrow = document.querySelector(".carousel__arrow_right");
+  let leftArrow = document.querySelector(".carousel__arrow_left");
+  let inner = document.querySelector(".carousel__inner");
+  let width = inner.offsetWidth;
+  let slides = document.querySelectorAll(".carousel__slide");
+  let position = 0;
+
+  if (position === 0) {
+    leftArrow.style.display = "none";
+  }
+
+  rightArrow.addEventListener("click", evt => {
+    inner.style.transform = "translateX(-" + (width * (position + 1)) + "px)";
+    position++;
+    if (position > 0) {
+      leftArrow.style.display = "";
+    }
+    if (position === slides.length - 1) {
+      rightArrow.style.display = "none";
+    }
+  });
+
+  leftArrow.addEventListener("click", evt => {
+    inner.style.transform = "translateX(-" + (width * (position - 1)) + "px)";
+    position--;
+    if (position < slides.length - 1) {
+      rightArrow.style.display = "";
+    }
+    if (position === 0) {
+      leftArrow.style.display = "none";
+    }
+  });
 }
