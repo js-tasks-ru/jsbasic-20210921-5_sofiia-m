21132bd4adf070a456546d0fa9416923d9f0b411
sonyawender <sonya.wender@gmail.com>
2021-10-24 20:41:49 +0300

    Tasks in Modules 4-5 solved

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,14 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement("ul");
+
+  for (const friend of friends) {
+    let li = document.createElement("li");
+    let name = [];
+    for (let key in friend) {
+      name.push(friend[key]);
+    }
+    li.textContent = name.join(" ");
+    ul.append(li);
+  }
+  return ul;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,28 @@
 function highlight(table) {
-  // ваш код...
+  let elements = table.getElementsByTagName("tr");
+  for (const element of elements) {
+    let childNodes = element.children;
+    for (let child of childNodes) {
+      let isAvailable = child.getAttribute("data-available");
+      if (isAvailable === "true") {
+        element.classList.add("available");
+      } else if (isAvailable === "false") {
+        element.classList.add("unavailable");
+      } else if (isAvailable == null) {
+        element.setAttribute("hidden", "hidden");
+      }
+
+      let textContent = child.textContent;
+
+      if (textContent === "m") {
+        element.classList.add("male");
+      } else if (textContent === "f") {
+        element.classList.add("female");
+      }
+
+      if (textContent < 18) {
+        element.style.textDecoration = "line-through";
+      }
+    }
+  }
 }
